e8a14324d484a918acb7ca4276547310944a6e2c
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-10-26 03:16:24 +0530

    Connected Whatsapp

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 import os
 import time
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
+from starlette.responses import Response
 
+from src.client.whatsapp import WhatsAppClient
 from src.router.game import game_router
 from src.router.message import message_router
 from src.router.user import user_router
+from src.service.whatsapp import handle_message
 from src.utils.client import getDBClient, getFirebaseClient
 
-app = FastAPI(title="BrainWave Backend", version="0.2.0-dev0")
+app = FastAPI(title="BrainWave Backend", version="0.2.0-dev1")
 
 origins = os.environ["CORS_ORIGINS"].split(",")
-
+ENDPOINT_GET_WHATSAPP_MESSAGES = "/whatsapp"
 
 class TimingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
@@ -68,6 +71,23 @@ app.include_router(user_router)
 app.include_router(message_router)
 app.include_router(game_router)
 
+@app.get(ENDPOINT_GET_WHATSAPP_MESSAGES)
+async def verify_webhook(request: Request):
+    VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
+    verify_token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+    if verify_token == VERIFY_TOKEN:
+        return Response(content=challenge, status_code=200)
+    else:
+        return Response(status_code=403)
+
+
+@app.post(ENDPOINT_GET_WHATSAPP_MESSAGES)
+async def get_whatsapp_messages(
+    request: dict,
+    wb_client: WhatsAppClient = Depends(WhatsAppClient),
+):
+    return handle_message(request, wb_client)
 
 if __name__ == "__main__":
     import uvicorn
