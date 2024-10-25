f031c3ca95deb2e40070096561780684769754cb
rzzdr <rudrpsingh@proton.me>
2024-10-25 23:44:28 +0530

    Fixed Errors

--- src/client/model/__init__.py
+++ src/client/model/__init__.py
@@ -1,17 +1,18 @@
-import os
 from src.client.model.chatbot import chat
 from src.client.model.config import system_prompt
-
 
 class Model:
     def __init__(self) -> None:
         self.system_prompt = system_prompt
 
-    def chatbot(self, query: str,  history: list[str]) -> list[str]:
+    def chatbot(self, query: str, history: list[str]) -> list[str]:
         sys = self.system_prompt
-        if history[0] != sys:
+        if history == [] or history[0] != sys:
             history.insert(0, sys)
-        history.append("user: "+query)
-        response = chat(query)
-        history.append("assistant: "+response)
+        history.append("user: " + query)
+        final_query = ""
+        for i in history:
+            final_query += i + "\n"
+        response = chat(final_query)
+        history.append("assistant: " + response)
         return history
